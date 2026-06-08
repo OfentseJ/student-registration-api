76750d80f4d1fc66337b2be3d1bc76c8188706e6
Ofentse <ofentsemakhutja@icloud.com>
2026-06-08 08:28:19 +0200

    first commit

---
+++ src/main/java/com/student/api/StudentRegistration/model/Student.java
@@ -0,0 +1,64 @@
+package com.student.api.StudentRegistration.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(name = "parent_phone", nullable = false)
+    private String parentPhone;
+
+    @Column(name = "parent_email", nullable = false)
+    private String parentEmail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentPhone() {
+        return parentPhone;
+    }
+
+    public void setParentPhone(String parentPhone) {
+        this.parentPhone = parentPhone;
+    }
+
+    public String getParentEmail() {
+        return parentEmail;
+    }
+
+    public void setParentEmail(String parentEmail) {
+        this.parentEmail = parentEmail;
+    }
+}
